7ebdbcf368e26da3a53768cefc4c5e87eab41b09
Denis Oliveira <denisb.oliveira@live.com>
2021-08-19 22:57:32 -0300

    Adicionar arquivos de projeto.

---
+++ Repository/CompanyRepository.cs
@@ -0,0 +1,52 @@
+﻿using DapperDemo.Data;
+using DapperDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperDemo.Repository
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CompanyRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Company Add(Company company)
+        {
+            _context.Companies.Add(company);
+            _context.SaveChanges();
+            return company;
+
+        }
+
+        public Company Get(int id)
+        {
+            return _context.Companies.Find(id); // opção 1
+            //return _context.Companies.FirstOrDefault(x => x.CompanyId == id); // opção 2
+        }
+
+        public List<Company> GetAll()
+        {
+            return _context.Companies.ToList();
+        }
+
+        public void Remove(int id)
+        {
+            var company =  _context.Companies.Find(id);
+            _context.Companies.Remove(company);
+            _context.SaveChanges();
+        }
+
+        public Company Update(Company company)
+        {
+            _context.Companies.Update(company);
+            _context.SaveChanges();
+            return company;
+        }
+    }
+}
